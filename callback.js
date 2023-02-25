6c232d5dd70ee5e348ea666a7c1eff4615e7ee4e
Yashika.U <97971705+Yashika24-U@users.noreply.github.com>
2023-02-25 11:41:25 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,62 @@
+const headingTag = document.createElement('h1');
+
+document.body.style.textAlign = 'center';
+
+document.body.append(headingTag);
+var countDown = 10;
+setTimeout(() => {
+    headingTag.innerText = '';
+    headingTag.innerText = countDown;
+    countDown--;
+    setTimeout(() => {
+        headingTag.innerText = '';
+        headingTag.innerText = countDown;
+        countDown--;
+        setTimeout(() => {
+            headingTag.innerText = '';
+            headingTag.innerText = countDown;
+            countDown--;
+            setTimeout(() => {
+                headingTag.innerText = '';
+                headingTag.innerText = countDown;
+                countDown--;
+                    setTimeout(() => {
+                    headingTag.innerText = '';
+                    headingTag.innerText = countDown;
+                    countDown--;
+                        setTimeout(() => {
+                        headingTag.innerText = '';
+                        headingTag.innerText = countDown;
+                        countDown--;
+                        setTimeout(() => {
+                            headingTag.innerText = '';
+                            headingTag.innerText = countDown;
+                            countDown--;
+                            setTimeout(() => {
+                            headingTag.innerText = '';
+                            headingTag.innerText = countDown;
+                            countDown--;
+                            setTimeout(() => {
+                                headingTag.innerText = '';
+                                headingTag.innerText = countDown;
+                                countDown--;
+                                setTimeout(() => {
+                                    headingTag.innerText = '';
+                                    headingTag.innerText = countDown;
+                                    countDown--;
+                                    setTimeout(() => {
+                                    headingTag.innerText = '';
+                                    headingTag.innerText = 'Happy Independence Day';
+                                    countDown--;
+                                }, 1000);
+                            }, 1000);
+                            }, 1000);
+                        }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
+Footer
